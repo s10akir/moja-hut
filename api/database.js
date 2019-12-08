aecb160e5a612f797bb22511dee6dc9423389739
Akira Shinohara <k017c1067@it-neec.jp>
2019-12-08 18:25:52 +0900

    [fix] set timezone to JST

--- api/database.js
+++ api/database.js
@@ -6,7 +6,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQL_HOSTNAME,
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  database: process.env.MYSQL_DATABASE,
+  timezone: 'jst'
 })
 
 db.connect()
