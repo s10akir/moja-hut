11647e737359dcbbd2f1a5922af120f05936fc64
Akira Shinohara <k017c1067@it-neec.jp>
2019-12-11 20:19:33 +0900

    [new] pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,9 @@ module.exports = {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+
+    '@nuxtjs/pwa'
   ],
   /*
    ** Axios module configuration
